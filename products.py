59c460f121a687318d9aab064f3a8565ea32195d
cabustillo13 <cabustillo13@hotmail.com>
2021-04-17 17:46:11 -0300

    [Add] main files

---
+++ products.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Crear una seudo base de datos de productos para consultar
+products = [
+    {"nombre": "Laptop", "precio": 1000, "cantidad":10},
+    {"nombre": "Mouse", "precio": 400, "cantidad":15},
+    {"nombre": "Teclado", "precio": 650, "cantidad":8}
+]
+
+
